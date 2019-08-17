dc434a387bb890fca49d6cb2595e7f234116a7b6
leonardoeverson <llinharespinheiro@gmail.com>
2019-08-17 19:37:43 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/';
+$config['base_url'] = 'http://php-code-igniter-app.herokuapp.com/';
+
 
 /*
 |--------------------------------------------------------------------------
